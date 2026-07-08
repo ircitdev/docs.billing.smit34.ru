947cab80f3db78d5f13f60fa5d00fb09d25bd24e
ircitdev <ircitdev@users.noreply.github.com>
2026-07-08 14:54:56 +0300

    CRM: организация по умолчанию + автораспределение Неразобранного (build 1482)

--- js/version.js
+++ js/version.js
@@ -6,8 +6,8 @@
 (function () {
   window.SMIT_DOC = {
     version: '3.0.0',
-    build: '1443',
-    updated: '05.07.2026',
+    build: '1482',
+    updated: '08.07.2026',
     year: '2026',
     company: 'ООО «СмИТ»',
   };
