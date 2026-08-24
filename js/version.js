13521ed132a258c5dae6dc30fb5be0bdf8029144
ircitdev <ircitdev@users.noreply.github.com>
2026-08-24 06:51:10 +0300

    Почтовый сервер: веб-почта, брендинг календаря, подпись из профиля, адресная книга

--- js/version.js
+++ js/version.js
@@ -6,8 +6,8 @@
 (function () {
   window.SMIT_DOC = {
     version: '3.5.0',
-    build: '2045',
-    updated: '20.08.2026',
+    build: '2105',
+    updated: '24.08.2026',
     year: '2026',
     company: 'ООО «СмИТ»',
   };
