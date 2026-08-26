15a8bb210dc71828a6fe82da7986b481cc7e4710
ircitdev <ircitdev@users.noreply.github.com>
2026-08-26 03:01:09 +0300

    видео: обучающий ролик по модулю Фискализация (плеер в разделе + галерея)

--- js/version.js
+++ js/version.js
@@ -6,8 +6,8 @@
 (function () {
   window.SMIT_DOC = {
     version: '3.5.0',
-    build: '2178',
-    updated: '25.08.2026',
+    build: '2180',
+    updated: '26.08.2026',
     year: '2026',
     company: 'ООО «СмИТ»',
   };
