41578b8914a046a249486b124b61fdb822a69cf2
ircitdev <ircitdev@users.noreply.github.com>
2026-08-31 17:15:32 +0300

    IPTV/LFStream: режимы REST/CMS, whitelist, создание аккаунта

--- js/version.js
+++ js/version.js
@@ -6,8 +6,8 @@
 (function () {
   window.SMIT_DOC = {
     version: '3.5.0',
-    build: '2286',
-    updated: '30.08.2026',
+    build: '2356',
+    updated: '31.08.2026',
     year: '2026',
     company: 'ООО «СмИТ»',
   };
